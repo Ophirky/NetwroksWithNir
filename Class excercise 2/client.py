0a86d4d718698bb51f3d046ea311fcee2b991100
Ophirky <ophirnevo3@gmail.com>
2023-11-12 13:30:11 +0200

    12/11/23

--- Class excercise 2/client.py
+++ Class excercise 2/client.py
@@ -8,6 +8,7 @@
 import socket as sock
 import logging
 import os
+from typing import List
 
 # Constants #
 IP = "127.0.0.1"
@@ -17,7 +18,28 @@ MAX_PACKET = 1024
 LOG_FORMAT = "%(levelname)s | %(asctime)s | %(processName)s | %(msg)s"
 LOG_LEVEL = logging.DEBUG
 LOG_DIR = "logs"
-LOG_FILE = f"{LOG_DIR}/log.log"
+LOG_FILE = f"{LOG_DIR}/client_log.log"
+
+PROTOCOL_FORMAT = "{msg_len}|{msg}"
+
+
+def protocol_format(msg: str) -> str:
+    """
+    Formats the msg to fit the protocol
+    :param msg: the msg to send
+    :return: None
+    """
+    return PROTOCOL_FORMAT.format(msg_len=len(msg), msg=msg)
+
+
+def protocol_deformat(formatted_msg: str) -> List:
+    """
+    returns the separated msg in a list
+    :param formatted_msg: the protocol formatted msg
+    :return List[int, string]: {len, msg}
+    """
+    return formatted_msg.split("|")
+
 
 # Client code #
 def main() -> None:
@@ -29,19 +51,24 @@ def main() -> None:
 
         while True:
             # Getting user input #
-            user_in = input("Enter a command: ")
+            user_in = protocol_format(input("Enter a command: "))
 
-            # Validating input #
-            if len(user_in) > 4 or len(user_in) < 4:
+            # Validating #
+            if len(user_in) > 6:
                 print("Invalid input")
                 continue
 
+            # Sending the user input to the server #
             socket.send(user_in.encode())
 
-            server_ans = socket.recv(MAX_PACKET).decode()
+            # Receiving answer from server #
+            server_ans = protocol_deformat(socket.recv(MAX_PACKET).decode())
 
-            print(server_ans)
-            if server_ans == 'Disconnecting...':
+            # Printing the server answer #
+            print(server_ans[1])
+
+            # Checking exit protocol #
+            if server_ans[1] == 'Disconnecting...':
                 break
 
     except sock.error as err:
@@ -56,6 +83,10 @@ if __name__ == '__main__':
     if not os.path.isdir(LOG_DIR):
         os.makedirs(LOG_DIR)
     logging.basicConfig(format=LOG_FORMAT, filename=LOG_FILE, level=LOG_LEVEL)
+
+    # Automatic checks #
+    assert protocol_format("msg") == PROTOCOL_FORMAT.format(msg_len=len("msg"), msg="msg")
+    assert protocol_deformat(f"{len('msg')}|msg") == f"{len('msg')}|msg".split("|")
 
     # Running main code #
     main()
--- Class excercise 2/server.py
+++ Class excercise 2/server.py
@@ -9,23 +9,48 @@ import logging
 import os
 import random as ran
 import socket as sock
+from typing import List
 
 # Constants #
 IP = "0.0.0.0"
 PORT = 5500
-MAX_PACKET = 4
+MAX_PACKET = 10
 QUEUE_LEN = 1
 
 LOG_FORMAT = "%(levelname)s | %(asctime)s | %(processName)s | %(msg)s"
 LOG_LEVEL = logging.DEBUG
 LOG_DIR = "logs"
-LOG_FILE = f"{LOG_DIR}/log.log"
+LOG_FILE = f"{LOG_DIR}/server_log.log"
 
+ERR_INVALID_INPUT = "Must have 4 characters"
 ERR_UNKNOWN_COMMAND = "Unknown Command"
+
+PROTOCOL_FORMAT = "{msg_len}|{msg}"
+SERVER_NAME = "Parabot"
+
+
+# Protocol functions #
+def protocol_format(msg: str) -> str:
+    """
+    Formats the msg to fit the protocol
+    :param msg: the msg to send
+    :return: None
+    """
+    return PROTOCOL_FORMAT.format(msg_len=len(msg), msg=msg)
+
+
+def protocol_deformat(formatted_msg: str) -> List:
+    """
+    returns the separated msg in a list
+    :param formatted_msg: the protocol formatted msg
+    :return List[int, string]: {len, msg}
+    """
+    return formatted_msg.split("|")
 
 
 # Commands Class #
 class Commands:
+    global SERVER_NAME
     commands = {
         "TIME": "Sends the time",
         "NAME": "Sends the command lines name",
@@ -43,17 +68,17 @@ class Commands:
     @staticmethod
     def comm_name() -> str:
         """
-        :return str: returns command lines' name
+        :return str: returns command lines name
         """
-        return "Johnny"
+        return SERVER_NAME
 
     @staticmethod
     def comm_rand() -> int:
         """
-        rolls a d6 dice
-        :return int: number between 1 & 6
+        returns a number between 1 - 10
+        :return int: number between 1 - 10
         """
-        return ran.randint(1, 6)
+        return ran.randint(1, 10)
 
     @staticmethod
     def comm_exit(client_socket: sock.socket) -> None:
@@ -61,8 +86,45 @@ class Commands:
         Closes the program
         :return: None
         """
-        client_socket.send("Disconnecting...".encode())
+        client_socket.send(protocol_format("Disconnecting...").encode())
         client_socket.close()
+
+    @staticmethod
+    def command_validate_and_run(client_socket: tuple, socket: sock.socket) -> None:
+        """
+        Handles the client input
+        :param client_socket: tuple containing (client_ip: str, client_socket: sock.socket)
+        :param socket:
+        :return:
+        """
+        while True:
+            user_input = protocol_deformat(client_socket[1].recv(MAX_PACKET).decode().upper())
+            print(user_input)
+            match user_input[1]:
+                # TIME - sends back the current time #
+                case 'TIME':
+                    client_socket[1].send(protocol_format(Commands.comm_time()).encode())
+
+                # NAME - sends back the name of the command prompt #
+                case 'NAME':
+                    client_socket[1].send(protocol_format(Commands.comm_name()).encode())
+
+                # RAND - sends back a random number between 1 & 10 #
+                case 'RAND':
+                    client_socket[1].send(protocol_format(str(Commands.comm_rand())).encode())
+
+                # EXIT - disconnects the client from the server #
+                case 'EXIT':
+                    Commands.comm_exit(client_socket[1])
+                    print(f"client disconnected: {client_socket[0]}")
+                    break
+
+                # Default - unknown command #
+                case _:
+                    if int(user_input[0]) != 4:
+                        client_socket[1].send(protocol_format(ERR_INVALID_INPUT).encode())
+                    else:
+                        client_socket[1].send(protocol_format(ERR_UNKNOWN_COMMAND).encode())
 
 
 def client_handle(socket: sock.socket):
@@ -73,41 +135,21 @@ def client_handle(socket: sock.socket):
     """
     # waiting for client connection & getting client info #
     socket.listen(QUEUE_LEN)
-    client_socket, client_ip = socket.accept()
+    while True:
+        client_socket, client_ip = socket.accept()
+        print(f"client connected: {client_ip}")
 
-    # Sending the command list #
-    client_socket.send(f"{Commands.commands=}".encode())
+        # Sending the command list #
+        client_socket.send(f"{Commands.commands=}".encode())
 
-    try:
-        while True:
-            # Command handling #
-            match client_socket.recv(MAX_PACKET).decode().upper():
-                # TIME - sends back the current time #
-                case 'TIME':
-                    client_socket.send(Commands.comm_time().encode())
+        try:
+            Commands.command_validate_and_run((client_ip, client_socket), socket)
 
-                # NAME - sends back the name of the command prompt #
-                case 'NAME':
-                    client_socket.send(Commands.comm_name().encode())
+        except Exception as err:
+            logging.exception(err)
 
-                # RAND - sends back a random number between 1 & 10 #
-                case 'RAND':
-                    client_socket.send(str(Commands.comm_rand()).encode())
-
-                # EXIT - disconnects the client from the server #
-                case 'EXIT':
-                    Commands.comm_exit(client_socket)
-                    client_handle(socket)
-
-                # Default - unknown command #
-                case _:
-                    client_socket.send(ERR_UNKNOWN_COMMAND)
-
-    except Exception as err:
-        logging.exception(err)
-
-    finally:
-        client_socket.close()
+        finally:
+            client_socket.close()
 
 
 # Main Server Code #
@@ -135,6 +177,10 @@ if __name__ == '__main__':
     if not os.path.isdir(LOG_DIR):
         os.makedirs(LOG_DIR)
     logging.basicConfig(format=LOG_FORMAT, filename=LOG_FILE, level=LOG_LEVEL)
+
+    # Auto Checks #
+    assert Commands.comm_name() == SERVER_NAME, "Name command not working"
+    assert 11 > Commands.comm_rand() > 0, "Rand command not failed"
 
     # Running main code #
     main()
